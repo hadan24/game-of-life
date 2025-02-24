caa06d638917a4fe568ca28d399ac2643c3e6e30
hadan24 <ha.dan.t02@gmail.com>
2025-02-23 21:55:16 -0800

    Raylib w/ ImGui test

--- src/game-of-life.cpp
+++ src/game-of-life.cpp
@@ -1,6 +1,5 @@
-#include <iostream>
 #include "imgui/imgui.h"
-#include "rlimgui/imgui_impl_raylib.h"
+#include "rlimgui/rlimgui.h"
 #include "raylib.h"
 
 int main()
@@ -8,13 +7,23 @@ int main()
     const int w = 700;
     const int h = 700;
     InitWindow(w, h, "test");
+    SetTargetFPS(144);
+    rlImGuiSetup(true);
 
     while (!WindowShouldClose())
     {
         BeginDrawing();
         ClearBackground(RAYWHITE);
+        rlImGuiBegin();
+
+        ImGui::Begin("hallo :D", NULL);
+        ImGui::Text("hallo from ImGui :D");
+        ImGui::End();
+
+        rlImGuiEnd();
         EndDrawing();
     }
 
+    rlImGuiShutdown();
     CloseWindow();
 }
