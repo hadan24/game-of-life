8a1aa6b6676a937d9908c2e0a4f805bf48f6238a
hadan24 <ha.dan.t02@gmail.com>
2025-06-02 20:38:30 -0700

    added rlimgui submodule, deleted vcpkg stuff

--- vcpkg_installed/vcpkg/blds/raylib/x64-windows-dbg/raylib/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/projs/C++/game-of-life/vcpkg_installed/vcpkg/blds/raylib/src/5.5-966575b391.clean/src
-# Build directory: D:/projs/C++/game-of-life/vcpkg_installed/vcpkg/blds/raylib/x64-windows-dbg/raylib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
